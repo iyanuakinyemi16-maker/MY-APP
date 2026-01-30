ba701ebc350bb125f3b420b3eaa9aa1218becfc9
Adewale Akinyemi <optiwal2@yahoo.com>
2026-01-30 12:40:13 +0100

    fixed the math

--- project1/script.test.js
+++ project1/script.test.js
@@ -2,7 +2,7 @@
       return (a*b) + (c-d)
 
  }
-   const results= complexMaths(10,20,50,40)
+   const results= complexMaths(11,20,50,40)
    console.log(results)
     test('the expected result should be 210', ()=>{
         expect(results).toBe(210)
